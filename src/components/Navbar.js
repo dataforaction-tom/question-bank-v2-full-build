e8792ec763108e7377833becb39b7c2f9faef604
Tom C Watson <tom.watson@dataforaction.org.uk>
2025-01-16 13:38:57 +0000

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,47 +43,91 @@ const Navbar = () => {
 
   return (
     <header className="bg-gradient-to-r from-slate-950 to-sky-900 text-white py-4 shadow-md">
-      <div className="container mx-auto flex justify-between items-center px-4">
-        <h1 className="text-2xl sm:text-4xl font-bold">Question Bank V2</h1>
-        <div className="sm:hidden">
-          <button onClick={toggleMenu} className="text-2xl focus:outline-none">
-            {menuOpen ? <FaTimes /> : <FaBars />}
-          </button>
+      <div className="container mx-auto px-4 max-w-full">
+        <div className="flex justify-between items-center sm:hidden">
+          <h1 className="text-2xl sm:text-4xl font-bold">Question Bank V2</h1>
+          <div>
+            <button onClick={toggleMenu} className="text-2xl focus:outline-none">
+              {menuOpen ? <FaTimes /> : <FaBars />}
+            </button>
+          </div>
         </div>
-        <nav className={`sm:flex sm:items-center ${menuOpen ? 'block' : 'hidden'} w-full sm:w-auto`}>
-          <Link to="/question-overview" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
-            Questions
-          </Link>
-          {session ? (
-            <>
-              <Link to="/submit-question" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
-                Submit Question
+
+        <div className="hidden sm:flex flex-col lg:flex-row justify-between items-center">
+          <h1 className="text-4xl font-bold mb-4 lg:mb-0">Question Bank V2</h1>
+          <nav className="flex items-center flex-wrap justify-center gap-2">
+            <Link to="/question-overview" className="block px-3 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+              Questions
+            </Link>
+            {session ? (
+              <>
+                <Link to="/submit-question" className="block px-3 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                  Submit Question
+                </Link>
+                <Link to="/create-group" className="block px-3 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                  Create Group
+                </Link>
+                <Link to="/group-dashboard" className="block px-3 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                  Group Dashboard
+                </Link>
+                <Link to="/profile" className="block px-3 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                  Profile
+                </Link>
+                <button onClick={handleLogout} className="block px-3 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <Link to="/signin" className="block px-3 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                  Sign In
+                </Link>
+                <Link to="/signup" className="block px-3 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                  Sign Up
+                </Link>
+              </>
+            )}
+          </nav>
+        </div>
+
+        {menuOpen && (
+          <div className="sm:hidden">
+            <nav className={`block w-full sm:w-auto`}>
+              <Link to="/question-overview" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                Questions
               </Link>
-              <Link to="/create-group" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
-                Create Group
-              </Link>
-              <Link to="/group-dashboard" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
-                Group Dashboard
-              </Link>
-              <Link to="/profile" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition flex items-center">
-                Profile
-                
-              </Link>
-              <button onClick={handleLogout} className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
-                Logout
-              </button>
-            </>
-          ) : (
-            <>
-              <Link to="/signin" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
-                Sign In
-              </Link>
-              <Link to="/signup" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
-                Sign Up
-              </Link>
-            </>
-          )}
-        </nav>
+              {session ? (
+                <>
+                  <Link to="/submit-question" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                    Submit Question
+                  </Link>
+                  <Link to="/create-group" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                    Create Group
+                  </Link>
+                  <Link to="/group-dashboard" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                    Group Dashboard
+                  </Link>
+                  <Link to="/profile" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition flex items-center">
+                    Profile
+                    
+                  </Link>
+                  <button onClick={handleLogout} className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                    Logout
+                  </button>
+                </>
+              ) : (
+                <>
+                  <Link to="/signin" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                    Sign In
+                  </Link>
+                  <Link to="/signup" className="block sm:inline-block mt-2 sm:mt-0 sm:ml-4 px-4 py-2 text-lg font-bold text-[#f4f4f4] hover:text-yellow-300 rounded transition">
+                    Sign Up
+                  </Link>
+                </>
+              )}
+            </nav>
+          </div>
+        )}
       </div>
     </header>
   );
